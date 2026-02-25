64dcff31984e35f1091dc32f8bcbaa62dc72058f
Zack Gacnik <zgacnik@carroll.edu>
2026-02-24 23:23:37 -0700

    Basic HTML home page

---
+++ src/main/kotlin/com/strixtracker/controller/HomeController.kt
@@ -0,0 +1,13 @@
+package com.strixtracker.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HomeController {
+
+    @GetMapping("/")
+    fun home(): String {
+        return "home"
+    }
+}
